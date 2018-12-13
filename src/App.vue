86b0aca89b10b2713a2afff566f0fdc0ff138a4f
evanhanawalt <hanawalt.8@osu.edu>
2018-12-13 17:35:20 -0500

    commiting my initial work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <h1>Punk API</h1>
+    </div>
+
+    <button
+      class="button is-primary"
+      v-bind:disabled="disableButton"
+      @click="getBeers"
+    >Load Some Beers!</button>
+    <div class="beers columns is-multiline">
+      <BeerCard
+        class="column is-one-quarter"
+        v-for="beer in beers"
+        v-bind:key="beer.id"
+        v-bind="beer"
+      ></BeerCard>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import BeerCard from "./components/BeerCard.vue";
+const API_ENDPOINT = "https://api.punkapi.com/v2/beers";
+
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    BeerCard
+  },
+  data: function() {
+    return {
+      disableButton: false,
+      beers: [{}]
+    };
+  },
+  methods: {
+    getBeers: function() {
+      /* eslint-disable */
+      const self = this;
+      this.disableButton = true;
+
+      axios
+        .get(API_ENDPOINT)
+        .then(function(response) {
+          // handle success
+          self.beers = response.data;
+          console.log(response.data);
+          this.beers.push({id:1})
+        })
+        .catch(function(error) {
+          // handle error
+        });
+    }
   }
-}
+};
 </script>
 
-<style>
+<style >
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+</style>
+
+<style lang="scss">
+$primary: #e75a3a;
+@import "../node_modules/bulma/bulma.sass";
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.beers {
+  margin: 1em;
 }
 </style>
---
+++ src/components/BeerCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="box">
+          <img :src="image_url" alt="Placeholder image">
+          <div class="content">
+            <p>{{name}}</p>
+            <p>{{abv}} %</p>
+          </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BeerCard",
+  props: ["image_url", "abv", "id","name"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.image-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+div.content {
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
